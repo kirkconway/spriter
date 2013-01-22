09ab2c7f8bd38c8262eeb897b46ebefa060eda31
Heinrich Reich <trixter16@web.de>
2013-01-22 13:51:14 +0100

    Last but not least: Clean up...

--- spriter-plugin/src/main/java/com/discobeard/spriter/converters/Converter.java
+++
@@ -1,6 +0,0 @@
-package com.discobeard.spriter.converters;
-
-public interface Converter<F,T> {
-	
-	public T convert(F from);
-}
--- spriter-plugin/src/main/java/com/discobeard/spriter/mergers/Merger.java
+++
@@ -1,6 +0,0 @@
-package com.discobeard.spriter.mergers;
-
-public interface Merger <T, K, V>{
-
-	public V merge(T from1, K from2);
-}

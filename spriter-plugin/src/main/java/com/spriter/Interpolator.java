f0333cc6f69b7debd4cb95a95f9a097f529b5d6b
Heinrich Reich <trixter16@web.de>
2013-02-16 20:58:50 +0100

    Test commit

--- spriter-plugin/src/main/java/com/spriter/Interpolator.java
+++
@@ -1,8 +0,0 @@
-package com.spriter;
-
-public interface Interpolator {
-	
-	float interpolate(float a, float b, float timeA, float timeB, long currentTime);
-	
-	float interpolateAngle(float a, float b, float timeA, float timeB, long currentTime);
-}
